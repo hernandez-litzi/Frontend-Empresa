b2f98dca13a07a7dcd3cff3797902518e896d220
Litzi Hernandez <litzihernandez@MacBook-Pro-de-Litzi.local>
2025-12-29 13:03:17 -0600

    Se creo el componente departamentos en la carpeta pages

---
+++ src/app/pages/departamentos/departamentos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-departamentos',
+  imports: [],
+  templateUrl: './departamentos.html',
+  styleUrl: './departamentos.css',
+})
+export class Departamentos {
+
+}
